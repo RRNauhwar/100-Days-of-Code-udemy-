e1cf1e6dffd75d76d16d668e235c29e9dc78ea0c
Rahul Chaudhary <155357957+RRNauhwar@users.noreply.github.com>
2025-07-02 01:05:57 +0530

    Update scoreboard.py

--- SNAKEGAME/scoreboard.py
+++ SNAKEGAME/scoreboard.py
@@ -8,18 +8,30 @@ class ScoreBoard(Turtle):
         self.goto(0, 260) 
         self.score = 0
         self.highest_score = 0
+        self.get_highest_score()
         self.write_score()
     def write_score(self):
         self.clear()  # Clear previous score before writing the new one
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score} Highest score:{self.highest_score}", align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.write_score()
+
+    def reset_highest_score(self):
+        if self.score>self.highest_score:
+            self.highest_score = self.score
 
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align="center", font=("Arial", 40, "bold"))
         self.goto(0, -40)
         self.write(f"Final Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.reset_highest_score()
         self.score = 0
+        with open("highest_score.txt",mode = "w") as file:
+            file.write(str(self.highest_score))
+
+    def get_highest_score(self):
+        with open("highest_score.txt",mode="r") as file:
+            self.highest_score = int(file.read())
