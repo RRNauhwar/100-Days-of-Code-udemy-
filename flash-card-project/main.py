0b5e5a21fe30489cf663aff740a78cd3d28b4712
Rahul Chaudhary <155357957+RRNauhwar@users.noreply.github.com>
2025-09-08 00:59:54 +0530

    Update main.py

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -3,23 +3,39 @@ import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 current_card = {}
- 
+flip_timer = 3000
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer) 
     current_card = random.choice(to_learn)
     print(current_card["French"])
     canvas.itemconfig(card_title,text="French",fill="black")
-    canvas.itemconfig(card_word,text=current_card["French"])
+    canvas.itemconfig(card_word,text=current_card["French"],fill="black")
     canvas.itemconfig(card_background,image=card_front_img)
-    window.after(3000,func=flip_card)
+    flip_timer = window.after(3000,func=flip_card)
 
 def flip_card():
     canvas.itemconfig(card_title,text="English",fill="white")
     canvas.itemconfig(card_word,text=current_card["English"])
     canvas.itemconfig(card_background,image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv",index=False)
+    next_card()
 
 # UI setup 
 window = Tk()
@@ -43,11 +59,10 @@ unknown_button = Button(image=cross_image,highlightthickness=0,command=next_card
 unknown_button.grid(row=1,column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image,highlightthickness=0,command=next_card)
+known_button = Button(image=check_image,highlightthickness=0,command=is_known)
 known_button.grid(row=1,column=1)
 
 next_card() #this is to show the first card when the program startsf
-
 
 
 window.mainloop()   
