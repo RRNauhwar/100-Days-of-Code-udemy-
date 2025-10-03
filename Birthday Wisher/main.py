14627d63543b409f68c6d23302cc185f330e2d61
Rahul Chaudhary <155357957+RRNauhwar@users.noreply.github.com>
2025-10-04 03:07:54 +0530

    Update main.py

--- Birthday Wisher/main.py
+++ Birthday Wisher/main.py
@@ -1,10 +1,13 @@
 import smtplib
 
-my_email = "rahulch19905@gmail.com"
+my_email = ""
 password = ""
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email,password=password)
-connection.sendmail(from_addr=my_email,to_addrs="rahulch19905@yahoo.com",msg="Hello")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com", 587) as connection: 
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="@yahoo.com",
+        msg="subject: Hello\n\nThis is the body of my email."
+        )
